0058f53f77672e17cd8342175af82529f24d7983
Michael Gao <mgao.ucla@gmail.com>
2011-11-04 03:33:12 -0700

    Initial Commit

---
+++ QRreader/QRreaderAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  QRreaderAppDelegate.h
+//  QRreader
+//
+//  Created by Michael Gao on 11/4/11.
+//  Copyright (c) 2011 UCLA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QRreaderAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ QRreader/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  QRreader
+//
+//  Created by Michael Gao on 11/4/11.
+//  Copyright (c) 2011 UCLA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "QRreaderAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([QRreaderAppDelegate class]));
+    }
+}
